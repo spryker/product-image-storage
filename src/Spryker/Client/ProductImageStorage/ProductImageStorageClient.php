0730f0fcf3f746f1ac08123783dcb584add4612c
Sergey Sikachev <sergey.sikachev@spryker.com>
2018-09-07 00:47:08 +0300

    GLUE-142. Product Image Sets Rest API refactring.

--- src/Spryker/Client/ProductImageStorage/ProductImageStorageClient.php
+++ src/Spryker/Client/ProductImageStorage/ProductImageStorageClient.php
@@ -7,8 +7,46 @@
 
 namespace Spryker\Client\ProductImageStorage;
 
+use Generated\Shared\Transfer\ProductAbstractImageStorageTransfer;
+use Generated\Shared\Transfer\ProductConcreteImageStorageTransfer;
 use Spryker\Client\Kernel\AbstractClient;
 
+/**
+ * @method \Spryker\Client\ProductImageStorage\ProductImageStorageFactory getFactory()
+ */
 class ProductImageStorageClient extends AbstractClient implements ProductImageStorageClientInterface
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param int $idProductAbstract
+     * @param string $locale
+     *
+     * @return \Generated\Shared\Transfer\ProductAbstractImageStorageTransfer|null
+     */
+    public function findProductImageAbstractStorageTransfer($idProductAbstract, $locale): ?ProductAbstractImageStorageTransfer
+    {
+        return $this->getFactory()
+            ->createProductAbstractImageStorageReader()
+            ->findProductImageAbstractStorageTransfer($idProductAbstract, $locale);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param int $idProductConcrete
+     * @param string $locale
+     *
+     * @return \Generated\Shared\Transfer\ProductConcreteImageStorageTransfer|null
+     */
+    public function findProductImageConcreteStorageTransfer($idProductConcrete, $locale): ?ProductConcreteImageStorageTransfer
+    {
+        return $this->getFactory()
+            ->createProductConcreteImageStorageReader()
+            ->findProductImageConcreteStorageTransfer($idProductConcrete, $locale);
+    }
 }
